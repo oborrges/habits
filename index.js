d48445d76d2369a2cb2561573fce925bc378d58e
luis <tiborges23@gmail.com>
2023-01-23 06:21:10 -0300

    I started the process of saving the information in the browser

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
+//ativando botão 
+
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+  if(dayExists) {
+    alert('Dia já incluso! ❌')
+    return
+  }
+  alert('adicionado com sucesso! ✅')
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify
+  (nlwSetup.data))
+}
+
+/*
 const data = {
   estudo: [
     "01-01",
@@ -8,17 +31,11 @@ const data = {
     "01-03",
     "01-04",
     "01-05",
-    "01-06",
-    "01-07",
-    "01-08",
-    "01-09",
-    "01-10",
-    "01-11",
-    "01-12",
+    "01-06"
   ],
   trabalho: ["01-03", "01-04", "01-05"],
   agua: ["01-03", "01-04", "01-05"],
-}
+}*/
 
-nlwSetup.setData(data)
-nlwSetup.load()
+//nlwSetup.setData(data)
+//nlwSetup.load()
